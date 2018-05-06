388e8abc091da837c301d75eb5753307036e08c7
HermitF <fantasrulai@gmail.com>
2018-05-06 12:44:57 +0800

    more key

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     f = open('keyword2.txt', 'w', encoding = 'UTF-8')    # 也可使用指定路徑等方式，如： C:\A.txt
     f.write(" ")
     f.close()
-    for index in rank.top_index(10):
+    for index in rank.top_index(17):
         # chcp 65001
         # print(sys.stdin.encoding)
         # print(sents[index].encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
@@ -36,17 +36,17 @@ if __name__ == '__main__':
         f = open('keyword2.txt', 'a', encoding = 'UTF-8')    # 也可使用指定路徑等方式，如： C:\A.txt
         # f.write(sents[index] + "_NL_")
         for key in sents[index].split():
-            f.write(key + "__NL__")
+            f.write(key + "_NL_")
         f.close()
 
     keyword_rank = textrank.KeywordTextRank(doc)
     keyword_rank.solve()
-    for w in keyword_rank.top_index(10):
+    for w in keyword_rank.top_index(17):
         if w != '︰':
             # print(w.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
             f = open('keyword.txt', 'a', encoding = 'UTF-8')    # 也可使用指定路徑等方式，如： C:\A.txt
             for key in w.split():
-                f.write(key + "__NL__")
+                f.write(key + "_NL_")
             # f.write(w + "_NL_")
             f.close()
             # print(w.encode("utf8").decode("cp950", "ignore"))
